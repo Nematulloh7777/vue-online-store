11fff0850dfe366fe2497e786b0228b03206c797
Nematulloh <nematulloh111@gmail.com>
2024-09-28 22:57:35 +0500

    fixed drawe

--- src/components/drawer.vue
+++ src/components/drawer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
-    <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
+    <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8 max-h-screen overflow-y-auto">
         <drawerHead />
 
         <div v-if="!cartStore.totalPrice || cartStore.orderId" class="flex h-full items-center">
